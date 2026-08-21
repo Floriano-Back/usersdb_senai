a06918e30a713b87b719ea6367642222a397d3c7
Floriano <floriano@email.com>
2026-08-20 21:47:00 -0300

    Atualizações

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
+import 'dotenv/config';
 import express from 'express';
-import mysql2 from 'mysql2/promise';
+import mysql from 'mysql2/promise';
 
 const app = express();
-const port = 3000;
+const port = process.env.SERVER_PORT;
 
 app.use(express.json());
 
-const pool = mysql2.createPool({
-    host:'localhost',
-    user:'root',
-    password:'1234',
-    database:'users_db',
-    port:'3306',
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
     waitForConnections:true,
     connectionLimit: 10,
     queueLimit: 0
